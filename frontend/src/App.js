90f7a754872417971afa1c237ef8de513d634efd
Jae <fahadmallick99@gmail.com>
2023-08-22 05:05:34 +0530

    using axios to fetch api data from backnd

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import { Route } from 'react-router-dom';
+import HomePage from './Pages/HomePage';
+import ChatPage from './Pages/ChatPage';
 
 function App() {
   return (
-    <div className="App">
-      hello
+    <div className="App"> 
+      <Route exact path='/' component = {HomePage} />
+      <Route exact path='/chat' component = {ChatPage} />
     </div>
   );
 }

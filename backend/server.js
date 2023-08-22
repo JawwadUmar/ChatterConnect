b39f7be69fe7b3d2242b2b2b4197ea7a5d3e27a9
Jae <fahadmallick99@gmail.com>
2023-08-22 20:36:26 +0530

    created an api for one on one chat

--- backend/server.js
+++ backend/server.js
@@ -5,12 +5,14 @@ const connectDB = require("./config/db");
 const colors = require("colors");
 const userRoutes = require("./routes/userRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+const chatRoutes = require("./routes/chatRoutes")
 
 dotenv.config();
 connectDB();
 const app = express();
 app.use(express.json()); //to accept json data
 app.use("/api/user", userRoutes);
+app.use('/api/chat', chatRoutes);
 
 
 

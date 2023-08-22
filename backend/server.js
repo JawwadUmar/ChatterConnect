ffd05dc74fe7e8c95b87956469f470c1e5918ece
Jae <fahadmallick99@gmail.com>
2023-08-22 18:52:18 +0530

    user authentication, working signup and login

--- backend/server.js
+++ backend/server.js
@@ -3,27 +3,27 @@ const { chats } = require("./data/data");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const colors = require("colors");
+const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 dotenv.config();
 connectDB();
 const app = express();
+app.use(express.json()); //to accept json data
+app.use("/api/user", userRoutes);
+
+
 
 app.get('/', (req, res) =>{
     res.send("API is running");
 
-})
+});
 
-app.get('/api/chat', (req, res) =>{
-    res.send(chats);
+// Error Handling middlewares
+app.use(notFound);
+app.use(errorHandler);
 
-})
-
-app.get('/api/chat/:id', (req, res) =>{
-    // console.log(req.params.id);
-    const singleChat = chats.find(c=>c._id ===req.params.id);
-    res.send(singleChat);
-})
 
 const PORT = process.env.PORT || 5000;
-
+ 
 app.listen(PORT, console.log(`Server Started on Port ${PORT}`.yellow.bold));
--- frontend/src/components/Authentication/Login.js
+++ frontend/src/components/Authentication/Login.js
@@ -1,18 +1,78 @@
-import { Button, FormControl, FormLabel, Input, InputGroup, InputRightElement, VStack } from '@chakra-ui/react'
-import React, { useState } from 'react'
+import { Button } from "@chakra-ui/button";
+import { FormControl, FormLabel } from "@chakra-ui/form-control";
+import { Input, InputGroup, InputRightElement } from "@chakra-ui/input";
+import { VStack } from "@chakra-ui/layout";
+import { useState } from "react";
+import axios from "axios";
+import { useToast } from "@chakra-ui/react";
+import { useHistory } from "react-router-dom";
 
 const Login = () => {
 
     const [show, setShow] = useState(false);
     const [email, setEmail] = useState();
     const [password, setPassword] = useState();
+    const [loading, setLoading] = useState(false);
+    const history = useHistory();
+    const toast = useToast();
+    // const { setUser } = ChatState();
 
     // const [picLoading, setPicLoading] = useState(false);
 
     const handleClick = () => setShow(!show);
 
 
-    const submitHandler = () =>{};
+    const submitHandler = async () => {
+      setLoading(true);
+      if (!email || !password) {
+        toast({
+          title: "Please Fill all the Feilds",
+          status: "warning",
+          duration: 5000,
+          isClosable: true,
+          position: "bottom",
+        });
+        setLoading(false);
+        return;
+      }
+  
+      try {
+        const config = {
+          headers: {
+            "Content-type": "application/json",
+          },
+        };
+  
+        const { data } = await axios.post(
+          "/api/user/login",
+          { email, password },
+          config
+        );
+  
+        toast({
+          title: "Login Successful",
+          status: "success",
+          duration: 5000,
+          isClosable: true,
+          position: "bottom",
+        });
+        localStorage.setItem("userInfo", JSON.stringify(data));
+        setLoading(false);
+        history.push("/chats");
+      } catch (error) {
+        toast({
+          title: "Error Occured!",
+          description: error.response.data.message,
+          status: "error",
+          duration: 5000,
+          isClosable: true,
+          position: "bottom",
+        });
+        setLoading(false);
+      }
+    };
+
+
   return (
     <VStack spacing='5px'>
        
@@ -20,6 +80,7 @@ const Login = () => {
             <FormLabel>Email</FormLabel>
             <Input 
             placeholder='Enter your Email'
+            value={email}
             onChange={(e) => setEmail(e.target.value)}/>
         </FormControl>
 
@@ -28,7 +89,8 @@ const Login = () => {
             <InputGroup>
             <Input 
             type={show? "text" : 'password'}
-            placeholder='Enter your Email'
+            placeholder='Enter your Password'
+            value={password}
             onChange={(e) => setPassword(e.target.value)}/>
 
             <InputRightElement width="4.5rem">
@@ -44,6 +106,7 @@ const Login = () => {
         width="100%"
         style={{ marginTop: 15 }}
         onClick={submitHandler} 
+        isLoading = {loading}
       >
         Login
       </Button>
